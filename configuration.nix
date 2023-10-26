992558ea2ed43c346c3f85ad345dfacdd50ebac7
Kaya-Sem <kayasemvc@gmail.com>
2023-10-26 10:05:38 +0200

    update annoying bluetooth blueman

--- configuration.nix
+++ configuration.nix
@@ -17,8 +17,7 @@
          
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
-  services.syncthing.enable = true;
+  
 
   # Adds an alias for editing this configfile.
   environment.interactiveShellInit = ''
