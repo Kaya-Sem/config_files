5f57996becc0d603e14d69f947795548aa89160a
Kaya-Sem <kayasemvc@gmail.com>
2023-11-02 18:32:39 +0100

    udpates
    + yakuake
    + startupscript

--- configuration.nix
+++ configuration.nix
@@ -56,10 +56,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = false;
-  services.xserver.desktopManager.gnome.enable = false;
-
   # Configure keymap in X11
   services.xserver = {
     layout = "be";
@@ -103,7 +99,7 @@
     spotify
     postgresql
     virtualbox
-   
+   libsForQt5.yakuake   
     ];
   };
 
